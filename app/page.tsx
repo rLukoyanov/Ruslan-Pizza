90786789367f8091e536d6320862efa2d37dea7d
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-08-04 20:20:47 +0400

    add filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import {
   Container,
+  Filters,
   Title,
-  Categories,
-  SortPopup,
   TopBar,
 } from "@/components/shared";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -15,6 +13,17 @@ export default function Home() {
       </Container>
 
       <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
